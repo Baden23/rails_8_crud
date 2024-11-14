000283a4b0b2461ab9843adcbbaa3b405c2fff45
baden <baden.rushworth@gmail.com>
2024-11-14 21:25:02 +0800

    destroy/delete kink_to (turbo_method: :delete) now works, no confirm prompt

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Import Turbo (for handling Turbo links)
 import "@hotwired/turbo-rails"
+
+// Import Stimulus controllers
 import "controllers"
+
+// Import Stimulus for the front-end framework
+import { Application } from "@hotwired/stimulus"
+const application = Application.start()
+
+// Configure Stimulus
+application.debug = false
+window.Stimulus = application
+
+export { application }
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,15 @@
-import { Application } from "@hotwired/stimulus"
+// Import Turbo (for handling Turbo links)
+import "@hotwired/turbo-rails"
 
+// Import Stimulus controllers
+import "controllers"
+
+// Import Stimulus for the front-end framework
+import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 
-// Configure Stimulus development experience
+// Configure Stimulus
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
